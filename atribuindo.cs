3b453cd7f43bf45968dd274b3fae090549477db0
gabbrieln <76093687+gabbrieln@users.noreply.github.com>
2021-08-26 08:38:20 -0300

    Atribuição.
    Atribuindo valor à idade conforme o pedido do usuário.

---
+++ atribuindo.cs
@@ -0,0 +1,14 @@
+using System;
+class Pessoa1{
+    static void Main(){
+        int idade = 0;
+        int qtdanos = 0;
+        int ano = 2021;
+        Console.WriteLine("Digite sua idade");
+        idade = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Sua idade ano que vem será {0}", idade + 1);
+        Console.WriteLine("Agora me diga daqui quantos anos você quer saber sua idade");
+        qtdanos = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Sua idade daqui {0} anos será {1} no ano de {2}", qtdanos, idade + qtdanos, qtdanos + ano);
+    }
+}
